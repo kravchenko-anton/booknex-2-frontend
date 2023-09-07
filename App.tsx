03aa19bfde0f4218d12abfdf0394e321d06cb78e
Anton-Kravkenko <antonkzavcenco300@gmail.com>
2023-09-07 10:12:07 +0200

    do from my account

--- App.tsx
+++ App.tsx
@@ -25,7 +25,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#101010',
+    backgroundColor: '#121212',
     alignItems: 'center',
     justifyContent: 'center',
   },
