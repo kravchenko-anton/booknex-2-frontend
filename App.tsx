9af6e2cab68e343eb665050bae8c1fecdc4d5af0
hii24 <sserhivalko@gmail.com>
2023-09-07 10:07:27 +0200

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+// color: dark["101010"] gray: [242424, 303030]
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{
+        color: '#fff',
+      }}>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="light" />
+      
+      <View style={{
+        width: 150,
+        height: 150,
+        backgroundColor: '#242424',
+        borderRadius: 20,
+      }}>
+      
+      </View>
     </View>
   );
 }
@@ -13,7 +25,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#101010',
     alignItems: 'center',
     justifyContent: 'center',
   },
