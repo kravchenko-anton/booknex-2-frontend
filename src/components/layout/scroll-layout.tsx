50b8add1b0a8ca1ae52b5e6fb7f032591fb778f1
Anton-Kravkenko <antonkzavcenco300@gmail.com>
2023-09-15 09:36:54 +0200

    add scroll-layout, fix icon color, add in home screen editions

---
+++ src/components/layout/scroll-layout.tsx
@@ -0,0 +1,14 @@
+import { UViewProps } from '@/types/component-types'
+import { FC, memo, PropsWithChildren } from 'react'
+import { View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const Layout: FC<PropsWithChildren<UViewProps>> = ({ children, ...props }) => (
+	<SafeAreaView edges={['right', 'top', 'left']} className='flex-1'>
+		<View className='flex-1 p-2' {...props}>
+			{children}
+		</View>
+	</SafeAreaView>
+)
+
+export default memo(Layout)
